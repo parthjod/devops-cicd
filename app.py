b7740169276c103c34b9939722e68427df85fc24
Your Name <hlomello765@gmail.com>
2026-09-23 22:57:00 +0530

    feat : devops-cicd

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps Pipeline Running Successfully."
+
+@app.route("/status")
+def status():
+    return "Application Status: ONLINE"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
